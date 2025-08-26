4fc56621e81169d4d2269d250d1d4e5ebd644219
jellymiso <jellydevelops17@gmail.com>
2025-08-27 02:32:27 +0800

    update

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,3 @@
-#!/usr/bin/env bash
-# Install dependencies
+#!/bin/bash
 pip install -r requirements.txt
-
-# (Optional) run migrations if you use a real DB in production
-# python manage.py migrate --noinput
-
-# Collect static assets to STATIC_ROOT
 python manage.py collectstatic --noinput --clear
